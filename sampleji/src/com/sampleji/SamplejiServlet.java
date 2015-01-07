5e90765c6cb07a57b681bbfa44d8207b9a975e0f
sonudhakar <sonu.dhakar@a-cti.com>
2015-01-07 21:09:27 +0530

    Test commit

---
+++ sampleji/src/com/sampleji/SamplejiServlet.java
@@ -0,0 +1,13 @@
+package com.sampleji;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SamplejiServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
